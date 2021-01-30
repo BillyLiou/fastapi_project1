692045b4f6771c25feff2836cd964dcf561a772b
劉品軒 <billyliou@BillyLiu.local>
2021-01-31 01:06:47 +0800

    增加Login module

--- app/main.py
+++ app/main.py
@@ -25,6 +25,7 @@ from app.routers.animal import Animal
 from app.routers.vehicle import Vehicle
 from app.routers.schema_practice import Schema
 from app.routers.response_model_practice import Cake
+from app.routers.auth import Login
 
 
 app = FastAPI()
@@ -32,6 +33,7 @@ app.include_router(Animal.router)
 app.include_router(Vehicle.router)
 app.include_router(Schema.router)
 app.include_router(Cake.router)
+app.include_router(Login.router)
 
 # 寫入日誌 & 日誌相關格式及檔案名配置
 FORMAT = '[%(asctime)s] [%(levelname)s][%(module)s:%(lineno)d] %(message)s'
---
+++ app/routers/auth/Login.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+'''
+@File    :   Login.py
+@Time    :   2021/01/29 19:57:34
+@Author  :   Billy Liu
+@Version :   1.0
+@Contact :   billy@test.mail
+@Desc    :   None
+'''
+
+from fastapi import APIRouter
+from pydantic import BaseModel,EmailStr
+from typing import Optional
+import hashlib
+
+
+router = APIRouter()
+
+class UserIn(BaseModel):
+    username: str
+    userpassword: str
+    email: EmailStr
+    full_name: Optional[str] = None
+
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: Optional[str] = None
+
+class UserInDB(BaseModel):
+    username: str
+    hashed_password: str
+    email: EmailStr
+    full_name: Optional[str] = None
+
+
+
+def test_save_user(user_in: UserIn):
+    hashed_password = getMD5_password(user_in.userpassword)
+    user_in_db = UserInDB(**user_in.dict(),hashed_password=hashed_password)
+    print('User save! But fot test')
+    return user_in_db
+
+# 初步將password加上token -> fbdt加密
+def test_password_plus_token(raw_password: str):
+    print('raw password is : {}'.format(raw_password))
+    tokend_password = 'fbdt'+raw_password
+    print('tokend password is: {}'.format(tokend_password))
+    return tokend_password
+
+# 將password加上token之後又用hash加密
+def getMD5_password(raw_password: str):
+    tokend_password = test_password_plus_token(raw_password)
+    md5 = hashlib.md5()
+    md5.update(tokend_password.encode('utf-8'))
+    res = md5.hexdigest()
+    print('hash password is :{}'.format(res))
+    return res
+
+@router.post("/login/",response_model=UserOut)
+async def create_uesr(user_in: UserIn):
+    user_saved = test_save_user(user_in)
+    return user_saved
+
