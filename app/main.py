5dd7501036bdd55590e89616e3f6f5f7dea05bc8
劉品軒 <billyliou@BillyLiu.local>
2021-02-01 09:33:37 +0800

    新增跨域的允許配置

--- app/main.py
+++ app/main.py
@@ -29,7 +29,26 @@ from app.routers.auth import Login
 from app.routers.error_practice import Error
 
 
+from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+    "http://127.0.0.1"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(Animal.router)
 app.include_router(Vehicle.router)
 app.include_router(Schema.router)
--- app/routers/auth/Login.py
+++ app/routers/auth/Login.py
@@ -35,19 +35,12 @@ class UserInDB(BaseModel):
     full_name: Optional[str] = None
 
 
-
 def test_save_user(user_in: UserIn):
+    print("# 1")
     hashed_password = getMD5_password(user_in.userpassword)
     user_in_db = UserInDB(**user_in.dict(),hashed_password=hashed_password)
     print('User save! But fot test')
     return user_in_db
-
-# 初步將password加上token -> fbdt加密
-def test_password_plus_token(raw_password: str):
-    print('raw password is : {}'.format(raw_password))
-    tokend_password = 'fbdt'+raw_password
-    print('tokend password is: {}'.format(tokend_password))
-    return tokend_password
 
 # 將password加上token之後又用hash加密
 def getMD5_password(raw_password: str):
@@ -58,8 +51,16 @@ def getMD5_password(raw_password: str):
     print('hash password is :{}'.format(res))
     return res
 
+# 初步將password加上token -> fbdt加密
+def test_password_plus_token(raw_password: str):
+    print('raw password is : {}'.format(raw_password))
+    tokend_password = 'fbdt'+raw_password
+    print('tokend password is: {}'.format(tokend_password))
+    return tokend_password
+
 @router.post("/login/",response_model=UserOut)
 async def create_uesr(user_in: UserIn):
     user_saved = test_save_user(user_in)
+    # user_saved.full_name = 'OK!!'
     return user_saved
 
